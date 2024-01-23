36b61bf036b0578e7b66bd775bd5501e6b6218f2
Bahnik, Tomas <tomas.bahnik@ataccama.com>
2024-01-23 18:37:45 +0100

    resolve PYCPT_HOME with exception, share PYCPT_ARTEFACTS folder

--- shared/__init__.py
+++ shared/__init__.py
@@ -1,3 +1,12 @@
 import os
+from pathlib import Path
 
-PYCPT_HOME = os.getenv('PYCPT_HOME')
+if not os.getenv('PYCPT_HOME'):
+    raise Exception('PYCPT_HOME env var not set')
+
+PYCPT_HOME = Path(os.getenv('PYCPT_HOME')).resolve()
+if not PYCPT_HOME.exists():
+    raise Exception(f'PYCPT_HOME {PYCPT_HOME} does not exist')
+
+PYCPT_ARTEFACTS: Path = Path(PYCPT_HOME, '../cpt_artefacts').resolve()
+SLA_TABLES_FOLDER: Path = Path(PYCPT_HOME, 'sla_tables')

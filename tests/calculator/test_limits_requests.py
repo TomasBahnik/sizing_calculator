248eae0f575665ef117a6f55a956bd4dd01c69c0
Bahnik, Tomas <tomas.bahnik@ataccama.com>
2024-01-23 20:03:01 +0100

    test max of limit and request of both cpu/memory make dummy test pass

--- tests/calculator/test_limits_requests.py
+++ tests/calculator/test_limits_requests.py
@@ -9,7 +9,7 @@ from sizing.calculator import LimitsRequests
 
 @pytest.mark.unit
 class TestLimitRequest:
-    def test_something(self):
+    def test_max_limit_request(self):
         from sizing.data import DataLoader
         from metrics import POD_BASIC_RESOURCES_TABLE
         from metrics.model.tables import PortalPrometheus
@@ -17,12 +17,16 @@ class TestLimitRequest:
         sla_table = PortalPrometheus().get_sla_table(table_name=POD_BASIC_RESOURCES_TABLE)
         df: pd.DataFrame = data_loader.load_df(sla_table=sla_table)
         cpu = LimitsRequests(sla_table=sla_table, resource=CPU_RESOURCE, ns_df=df)
-        assert cpu.request_value.max() == 1
+        max_cpu_request = cpu.request_value.max()
+        assert max_cpu_request == 1
+        max_cpu_limit = cpu.limit_value.max()
+        assert max_cpu_limit == 1.5
         memory = LimitsRequests(sla_table=sla_table, resource=MEMORY_RESOURCE, ns_df=df)
         from metrics import MIBS
-        max_memory_mib = memory.request_value.max() / MIBS
-        # 4200 Mi
-        assert max_memory_mib == 4200
+        max_request_memory_mib = memory.request_value.max() / MIBS
+        assert max_request_memory_mib == 4200
+        max_limit_memory_mib = memory.limit_value.max() / MIBS
+        assert max_limit_memory_mib == max_request_memory_mib
 
 
 if __name__ == '__main__':

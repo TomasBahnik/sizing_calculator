3ff3842f884909196a4bb5d2df05e166b26aa85f
Bahnik, Tomas <tomas.bahnik@ataccama.com>
2024-01-24 19:23:07 +0100

    calculator imports all from sizing

--- sizing/calculator.py
+++ sizing/calculator.py
@@ -15,8 +15,7 @@ from metrics.model.tables import SlaTables
 from prometheus.sla_model import SlaTable
 from reports.html import sizing_calc_report, sizing_calc_summary_header
 from shared import PYCPT_ARTEFACTS
-from sizing import REQUEST_PERCENTILE, CPU_REQUEST_NAME, LIMIT_PERCENTILE, CPU_LIMIT_NAME, MEMORY_REQUEST_NAME, \
-    MEMORY_LIMIT_NAME, CPU_LIMIT_MILLIS_COLUMNS, MEMORY_LIMIT_MIBS_COLUMNS, PERCENTILES
+from sizing import *
 from sizing.rules import PrometheusRules
 from test_summary.model import TestDetails, TestSummary
 

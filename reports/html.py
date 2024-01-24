0fb98332536391930aa4aa69f907ccf20d837f11
Bahnik, Tomas <tomas.bahnik@ataccama.com>
2024-01-24 18:34:46 +0100

    sizing calc and sla reports to html.py

--- reports/html.py
+++ reports/html.py
@@ -1,23 +1,27 @@
+from __future__ import annotations
+
 import os
 from pathlib import Path
-from typing import Dict, List
+from typing import Dict, List, Optional
 
 import pandas as pd
 import typer
 
+from metrics import DEFAULT_STEP_SEC
 from metrics.collector import TimeRange
 from prometheus.sla_model import SlaTable
 from reports import PROMETHEUS_REPORT_FOLDER
+from test_summary.model import TestDetails, TestSummary
 
 
-def report_header(sla_table: SlaTable, time_range: TimeRange) -> str:
+def sla_report_header(sla_table: SlaTable, time_range: TimeRange) -> str:
     data: Dict[str, List[str]] = {'table_name': [sla_table.tableName],
                                   'time_range': [f'{time_range.from_time}-{time_range.to_time}']}
     df: pd.DataFrame = pd.DataFrame(data=data)
     return df.T.to_html()
 
 
-def save_rules_report(main_report: str, sla_table: SlaTable, time_range: TimeRange):
+def sla_report(main_report: str, sla_table: SlaTable, time_range: TimeRange):
     from shared.utils import DATE_TIME_FORMAT_FOLDER
     ft = time_range.from_time.strftime(DATE_TIME_FORMAT_FOLDER)
     tt = time_range.to_time.strftime(DATE_TIME_FORMAT_FOLDER)
@@ -28,3 +32,43 @@ def save_rules_report(main_report: str, sla_table: SlaTable, time_range: TimeRan
     typer.echo(msg)
     with open(html_file, "w") as file:
         file.write(main_report)
+
+
+def sizing_calc_report_header(test_details: Optional[TestDetails], time_range: TimeRange) -> str:
+    """report header with namespace and time range"""
+    # time_range present always
+    duration = (time_range.to_time - time_range.from_time).total_seconds()
+    samples = int(duration / DEFAULT_STEP_SEC) + 1
+    description = test_details.description if test_details else ''
+    data: Dict[str, List[str]] = {'description': [description],
+                                  'duration [hours]': [duration / 3600],
+                                  'from': [time_range.from_time.isoformat()],
+                                  'to': [time_range.to_time.isoformat()],
+                                  'max samples': [samples],
+                                  }
+    df: pd.DataFrame = pd.DataFrame(data=data)
+    return df.to_html()
+
+
+def sizing_calc_report(time_range: TimeRange, test_details: TestDetails,
+                       data: pd.DataFrame, folder: Path, file_name: str,
+                       test_summary: Optional[TestSummary] = None):
+    """Create and save full report to file"""
+    file_name = f'{file_name}_{str(time_range)}.html'
+    report_header = sizing_calc_report_header(test_details, time_range) \
+        if test_summary is None else sizing_calc_summary_header(test_summary) + "<br/>" + sizing_calc_report_header(test_details, time_range)
+    path = Path(folder, file_name)
+    msg = f'Saving report to {path.resolve()}'
+    typer.echo(msg)
+    with open(path, 'w') as f:
+        f.write(report_header + ("<br/>" + data.to_html() + "<hr>"))
+
+
+def sizing_calc_summary_header(test_summary: TestSummary) -> str:
+    """test summary header"""
+    data: Dict[str, List[str]] = {'name': [test_summary.name],
+                                  'namespace': [test_summary.namespace],
+                                  'catalog items': [test_summary.catalogItems],
+                                  }
+    df: pd.DataFrame = pd.DataFrame(data=data)
+    return df.to_html()
